9c1112e1bf95e820711838059716b9b1109d3dd8
David <davidruben14@gmail.com>
2020-06-25 14:15:52 +0200

    create function pillbox

--- src/components/pilule/Pilule.js
+++ src/components/pilule/Pilule.js
@@ -1,38 +1,80 @@
 import React from 'react'
 import './pilule.css'
+import {useState} from 'react'
+import imagePill from './assets/pillbox.png'
+
 
 const Pilule = () => {
 
+    const [pill,setPill] = useState([{
+        name: 'doliprane',
+        duree : 2,
+        morning:true
+    },{
+        name:'advil',
+        duree:3,
+        morning:false
+    },{
+        name:'smecta',
+        duree:6,
+        morning:true
+    }])
+const [pillBox,setPillBox] = useState([])
+
+   const dureeMax = () => {
+       let durMax = 0
+       pill.forEach(e => { 
+           if(e.duree > durMax){
+              return durMax = e.duree
+                
+           }
+      })
+       return durMax 
+   }
+
+console.log(dureeMax())
+   
+   let day = 1
+
+   for(let i = 1;i <= dureeMax();i++){
+       pillBox.push(<>
+       <h3 className="day">Day :{i}</h3>
+       {pill.map(x => {
+       return(
+           <>
+           { i <= x.duree &&    
+        <div className='tableau'>
+                <div className='container'>
+           <h4>{x.name}</h4>
+                </div>
+                <div className='container_day'>
+                    <div className='morning'>
+                    <p>Morning</p>
+                    {x.morning === true &&
+                      <img src={imagePill} alt='pill' width='50px'/>}
+                    </div>
+                    <div className='morning'>
+                    <p>Midday</p>
+                    <p>1 medoc</p>
+                    </div>
+                    <div className='morning'>
+                    <p>Evening</p>
+                    <p>1 medoc</p>
+                    </div>
+                    <div className='morning4'>
+                    <p>Night</p>
+                    <p>1 medoc</p>
+                    </div>
+                </div>
+           </div>}</>)})}
+       </>)
+       }
+    
 
     return(
-        <>
-        <h3 className="day">Day : 1</h3>
-        <div className='tableau'>
-            <div className='container'>
-                <h4>Doliprane</h4>
-            </div>
-            <div className='container_day'>
-                <div className='morning'>
-                <p>Morning</p>
-                <p>1 medoc</p>
-                </div>
-                <div className='morning'>
-                <p>Midday</p>
-                <p>1 medoc</p>
-                </div>
-                <div className='morning'>
-                <p>Evening</p>
-                <p>1 medoc</p>
-                </div>
-                <div className='morning4'>
-                <p>Night</p>
-                <p>1 medoc</p>
-                </div>
-                
-            </div>
-
+        <div>  
+            {pillBox}
         </div>
-        </>
     )
 }
 
