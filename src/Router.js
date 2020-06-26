6d62b0e7951e74a732637bb19f09a336b97b0e41
jeanfrancoisuccelli <jeanfrancoisuccelli@gmail.com>
2020-06-26 10:24:26 +0200

    pull elements from features

--- src/Router.js
+++ src/Router.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import { Switch ,Route } from 'react-router-dom'
-import Accueil from './components/accueil/Accueil'
-import InfosPatient from './components/infosPatient/InfosPatient'
-import FormOrder from './components/formOrder/FormOrder'
-import Order from './components/order/Order'
-import Pilule from './components/pilule/Pilule'
-import Rdv from './components/rdv/Rdv'
-import Footer from './components/footer/Footer'
-import BarNav from './components/barNav/BarNav'
-import Landing from './components/Landing/Landing'
-// import ChatBot from "./components/chatBot/ChatBot";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Accueil from "./components/accueil/Accueil";
+import InfosPatient from "./components/infosPatient/InfosPatient";
+import FormOrder from "./components/formOrder/FormOrder";
+import Order from "./components/order/Order";
+import Pilule from "./components/pilule/Pilule";
+import Rdv from "./components/rdv/Rdv";
+// import Chat from './components/chat/Chat'
+import BarNav from "./components/barNav/BarNav";
+import Landing from "./components/Landing/Landing";
 
-const Router = () =>{
-return (
-  <>
-    {/* <BarNav /> */}
-    <Landing />
-    <Switch>
-      {/* <Route exact path="/" component={Accueil} /> */}
-      <Route path="/FormOrder" component={FormOrder} />
-      <Route path="/Order" component={Order} />
-      {/* <Route path="/ChatBot" component={ChatBot} /> */}
-      <Route path="/Pilule" component={Pilule} />
-      {/* <Route path="/InfosPatient" component={InfosPatient} />
+const Router = () => {
+  return (
+    <>
+      <BarNav />
+      <Switch>
+        <Route exact path="/" component={Accueil} />
+        <Route path="/FormOrder" component={FormOrder} />
+        <Route path="/Order" component={Order} />
+        {/* <Route path="/Chat" component={Chat} /> */}
+        <Route path="/Pilule" component={Pilule} />
+        {/* <Route path="/InfosPatient" component={InfosPatient} />
       <Route path="/Rdv" component={Rdv} /> */}
-    </Switch>
-    {/* <Footer/>*/}
-  </>
-);
-}
-export default Router
-
-
-
+      </Switch>
+      {/* <Footer/>*/}
+    </>
+  );
+};
+export default Router;
